24985c564c2463f20b89e1e36d20c922add24739
AravDesai <145138457+AravDesai@users.noreply.github.com>
2024-07-21 23:46:51 +0530

    Changes in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eframe::egui::{
-    self, Align2, Button, Color32, Context, FontFamily, FontId, Id, LayerId, Pos2, Rect,
+    self, Align2, Button, Color32, Context, FontFamily, FontId, Id, LayerId, Pos2, Rect, Ui,
 };
 use egui_circle_trim::egui_circle_trim::CircleTrim;
 use lb_rs::{Config, Core, File, Uuid};
@@ -34,8 +34,10 @@ fn main() {
 struct MyApp {
     current_root: Data,
     inner_radius: f32,
+    outer_radius: f32,
     start_angle: i32,
     data: Vec<Data>,
+    repaint_check: bool,
 }
 
 impl MyApp {
@@ -49,27 +51,15 @@ impl MyApp {
                 root = item;
             }
         }
-
-        let mut sorted = vec![];
-        sorted.push(root);
-
         Self {
             data: data_clone,
-            current_root: sorted[0].clone(),
+            current_root: root,
             inner_radius: 50.0,
+            outer_radius: 70.0,
             start_angle: 0,
+            repaint_check: true,
         }
     }
-<<<<<<< Updated upstream
-}
-
-fn file_recurser(parent: Data, data: Vec<Data>, root: Data) {
-    let mut files = vec![];
-    for file in data {
-        if (file.file.parent == parent.file.id && file.file.id != root.file.id) {
-            files.push(file);
-=======
-
     fn file_recurser(&self, ui: &mut Ui,layer_id: i32, parent: Data, radius: f32, mut inner_bound: i32, outer_bound: i32, center: Pos2) {
         for file in &self.data {
             if file.file.parent == parent.file.id && file.file.id != self.current_root.file.id {
@@ -80,13 +70,13 @@ fn file_recurser(parent: Data, data: Vec<Data>, root: Data) {
 
                 if file.file.is_folder(){
                     self.file_recurser(ui, layer_id + 1, file.clone(), radius + 20.0, inner_bound, inner_bound + chunk, center);
-                }   
+                }
 
                 inner_bound+= chunk;
             }
->>>>>>> Stashed changes
         }
     }
+
 }
 
 impl eframe::App for MyApp {
@@ -94,7 +84,7 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             let window_size = ctx.input(|i: &egui::InputState| i.screen_rect());
 
-            ui.heading("Memory Viewer");
+            ui.heading("Storage Viewer");
 
             let center = Rect {
                 min: window_size.max / 2.0,
@@ -164,7 +154,6 @@ impl eframe::App for MyApp {
                 });
                 let trim = CircleTrim::new(Color32::BLUE, self.inner_radius, 0, 90, center.min);
 
-<<<<<<< Updated upstream
                 ui.allocate_ui_at_rect(CircleTrim::get_center_rect(&trim), |ui| {
                     ui.with_layer_id(
                         LayerId {
@@ -183,34 +172,6 @@ impl eframe::App for MyApp {
                     self.data.clone(),
                     self.current_root.clone(),
                 );
-=======
-                //ui.add(CircleTrim::new(Color32::RED, 50.0, 0, 90, center.min, 4));
-
-                let mut blue = CircleTrim::new(Color32::BLUE, 50.0, 0, 90, center.min, 20);
-
-                if blue.draw(ui).root_changed{
-
-                }
-
-
-                // ui.allocate_ui_at_rect(CircleTrim::get_center_rect(&trim), |ui| {
-                //     ui.with_layer_id(
-                //         LayerId {
-                //             order: egui::Order::Foreground,
-                //             id: Id::new(1),
-                //         },
-                //         |ui| {
-                //             ui.add(Button::new("").fill(Color32::WHITE).rounding(100.0).small())
-                //                 .on_hover_text("Test")
-                //         },
-                //     )
-                // });
-
-                // if (self.repaint_check) {
-                //     self.file_recurser(ui, 2, self.current_root.clone(), 50.0, 0, 360, center.min);
-                //     self.repaint_check = false;
-                // }
->>>>>>> Stashed changes
             });
         });
     }
