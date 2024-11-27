15b9176e0ceb1a035265b271349a24d9c9430893
Parth <parth@mehrotra.me>
2024-11-27 18:50:31 -0500

    some more progress

--- src/data.rs
+++ src/data.rs
@@ -24,8 +24,9 @@ pub struct FileRow {
 
 impl Data {
     pub fn init() -> Self {
-        let json_info = fs::read_to_string("parth-doc-data.json").expect("Couldn't read file");
-        let data: Vec<FileRow> = serde_json::from_str(&json_info).expect("Json not formatted well");
+        let file_contents = fs::read_to_string("parth-doc-data.json").expect("Couldn't read file");
+        let data: Vec<FileRow> =
+            serde_json::from_str(&file_contents).expect("Json not formatted well");
 
         let mut all_files = HashMap::new();
         let mut root = Uuid::nil();
@@ -39,15 +40,18 @@ impl Data {
         let mut folder_sizes = HashMap::new();
         for datum in data {
             let size = datum.size;
-            let current_id = datum.file.id;
+            let mut current_id = datum.file.id;
             loop {
                 let row = all_files.get(&current_id).unwrap();
-                let current_size = folder_sizes.get(&row.file.parent).copied().unwrap_or_default();
+                let current_size = folder_sizes
+                    .get(&row.file.parent)
+                    .copied()
+                    .unwrap_or_default();
                 folder_sizes.insert(row.file.parent, size + current_size);
                 if current_id == root {
                     break;
                 }
-                let current_id = row.file.parent;
+                current_id = row.file.parent;
             }
         }
 
@@ -58,12 +62,43 @@ impl Data {
         }
     }
 
-    pub fn set_root(&mut self, id: Uuid) {
-        self.current_root = id;
+
+
+    pub struct Layers {
+        layers: Vec<Vec<BetterNode>>
     }
 
-    pub fn get_children(&self) -> Vec<Node> {
-        // let overall_size = self.folder_sizes.get(&self.current_root);
+    pub struct BetterNode {
+        id: Uuid,
+        name: String,
+        portion: f64,
+    }
+
+    // populate Vec<Node>
+    // write some basic tests at the bottom and fix any mistakes in here
+    // all we're trying to accomplish by wed 4th is a very reliable data abstraction
+    pub fn get_children(&self, id: &Uuid) -> Vec<Node> {
+        let total_size = *self.folder_sizes.get(id).unwrap() as f64;
+        let children = self
+            .all_files
+            .values()
+            .filter(|f| f.file.parent == *id) // todo handle root
+            .map(|f| Node {
+                id: f.file.id,
+                name: f.file.name.clone(),
+                portion: f.size as f64 / total_size,
+                children: todo!(),
+            });
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Data;
+
+    #[test]
+    fn init() {
+        Data::init();
     }
 }
